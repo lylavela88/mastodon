e5c4a11a978c4734c70ff7faa5b4d867c56245f0
Eric Gautreaux <messenjahofchrist@gmail.com>
2020-09-03 23:21:51 -0500

    added source code link

--- app/javascript/mastodon/features/ui/components/link_footer.js
+++ app/javascript/mastodon/features/ui/components/link_footer.js
@@ -11,7 +11,7 @@ const LinkFooter = ({ withHotkeys }) => (
       {withHotkeys && <li><Link to='/keyboard-shortcuts'><FormattedMessage id='navigation_bar.keyboard_shortcuts' defaultMessage='Hotkeys' /></Link> · </li>}
       <li><a href='/auth/edit'><FormattedMessage id='getting_started.security' defaultMessage='Security' /></a> · </li>
       <li><a href='/about/more'><FormattedMessage id='navigation_bar.info' defaultMessage='About this server' /></a> · </li>
-      <li><a href='/dingdocs.html'><FormattedMessage id='Help Docs' /></a>  · </li>
+      <li><a href='/dingdocs.html'><FormattedMessage id='Help Docs' /></a>  · </li>      
       
       {/* <li><a href='https://joinmastodon.org/apps' target='_blank'><FormattedMessage id='navigation_bar.apps' defaultMessage='Mobile apps' /></a> · </li> */}
       <li><a href='/terms' target='_blank'><FormattedMessage id='getting_started.terms' defaultMessage='Terms of service' /></a> · </li>
@@ -20,13 +20,13 @@ const LinkFooter = ({ withHotkeys }) => (
       <li><a href='/auth/sign_out' data-method='delete'><FormattedMessage id='navigation_bar.logout' defaultMessage='Logout' /></a></li>
     </ul>
 
-    {/* <p>
+    <p>
       <FormattedMessage
         id='getting_started.open_source_notice'
-        defaultMessage='Mastodon is open source software. You can contribute or report issues on GitHub at {github}.'
-        values={{ github: <span><a href={source_url} rel='noopener' target='_blank'>{repository}</a> (v{version})</span> }}
+        defaultMessage='Ding Dash is open source software. You can contribute or report issues on GitHub at {github}.'
+        values={{ github: <span><a href='https://github.com/Origin-Creative/mastodon' rel='noopener' target='_blank'>{repository}</a> (v{version})</span> }}
       />
-    </p> */}
+    </p>
   </div>
 );
 
